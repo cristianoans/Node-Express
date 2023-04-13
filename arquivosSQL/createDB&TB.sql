0e4383a5c16f94fdf585f785d40bca642892cdbc
unknown <cristianoans@hotmail.com>
2023-04-12 22:18:35 -0300

    criado o meto put para atualização dos dados. foi incluido coluna matricula para controlar inserts duplicados

--- arquivosSQL/createDB&TB.sql
+++ arquivosSQL/createDB&TB.sql
@@ -4,6 +4,7 @@ USE `nodeexpressmysql`;
 
 CREATE TABLE IF NOT EXISTS `Usuarios` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `matricula` VARCHAR(20) UNIQUE NOT NULL,
   `NomeCompleto` VARCHAR(255) NOT NULL,
   `Nota1` DECIMAL(4, 2) NOT NULL,
   `Nota2` DECIMAL(4, 2) NOT NULL,
@@ -12,13 +13,13 @@ CREATE TABLE IF NOT EXISTS `Usuarios` (
 );
 
 
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Cristiano Oliveira',95,95,9.5,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Vivianne Silva',10.0,9.0,9.5,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('José Alves',6.0,9.0,7.5,0);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Vanda Rosa',7.0,9.0,8.0,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Karla Oliveira',8.5,8.0,8.25,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Keila Oliveira',8.0,9.0,8.5,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Juliane Rodrigues',6.5,9.0,7.75,0);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Gustavo Henrique',5.5,6.0,5.75,0);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Beatriz Eduarda',9.8,6.5,8.15,1);
-INSERT INTO `nodeexpressmysql`.`usuarios` (`NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('Victoria Oliveira',7.5,8.0,7.75,0);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('01012023','Vivianne Silva',10.0,9.0,9.5,1);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('02012023','José Alves',6.0,9.0,7.5,0);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('03012023','Vanda Rosa',7.0,9.0,8.0,1);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('04012023','Karla Oliveira',8.5,8.0,8.25,1);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('05012023','Keila Oliveira',8.0,9.0,8.5,1);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('06012023','Juliane Rodrigues',6.5,9.0,7.75,0);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('07012023','Gustavo Henrique',5.5,6.0,5.75,0);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('08012023','Beatriz Eduarda',9.8,6.5,8.15,1);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('09012023','Victoria Oliveira',7.5,8.0,7.75,0);
+INSERT INTO `nodeexpressmysql`.`usuarios` (`Matricula`, `NomeCompleto`, `Nota1`, `Nota2`, `Media`, `Aprovado`) VALUES ('10012023','Cristiano Oliveira',10.0,10.0,10.0,1);
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,49 +5,75 @@ const app = express();
 // app.use(express.json());
 const usuarios = express.Router();
 
+usuarios.route('/')
+    .get((req, res) => {
+        const { nome, media, aprovado } = req.query;
 
-usuarios.get('/', (req, res) => {
-    const { nome, media, aprovado } = req.query;
-
-    if (nome) {
-        pool.query(`SELECT * FROM usuarios WHERE NomeCompleto LIKE '%${nome}%'`, function (err, result, fields) {
-            res.json(result);
-        })
-    } else if (media) {
-        pool.query(`SELECT * FROM usuarios WHERE Media >= ${media}`, function (err, result, fields) {
-            res.json(result);
-        })
-    } else if (aprovado) {
-        pool.query(`SELECT * FROM usuarios WHERE Aprovado = ${aprovado}`, function (err, result, fields) {
-            res.json(result);
-        })
-    } else {
-        pool.query("SELECT * FROM `usuarios`", function (err, result, fields) {
-            res.json(result);
-        })
-    }
-
-});
-
-usuarios.post('/novo', (req, res) => {
-    const { nome, nota1, nota2 } = req.body;
-
-    if (nota1 && nota2 && nome) {
-        if (!isNaN(nota1) && !isNaN(nota2)) {
-            const media = ((parseFloat(nota1) + parseFloat(nota2)) / 2);
-    
-            pool.query(`INSERT INTO usuarios (NomeCompleto, Nota1, Nota2, Media, Aprovado) VALUES ('${nome}',${nota1},${nota2},${media},${aprovado()})`, function (err, result, fields) {
+        if (nome) {
+            pool.query(`SELECT * FROM usuarios WHERE NomeCompleto LIKE '%${nome}%'`, function (err, result, fields) {
+                res.json(result);
+            })
+        } else if (media) {
+            pool.query(`SELECT * FROM usuarios WHERE Media >= ${media}`, function (err, result, fields) {
+                res.json(result);
+            })
+        } else if (aprovado) {
+            pool.query(`SELECT * FROM usuarios WHERE Aprovado = ${aprovado}`, function (err, result, fields) {
                 res.json(result);
             })
         } else {
-            res.json({ error: 'Nota inválida, deve ser numérica.' });
+            pool.query("SELECT * FROM `usuarios`", function (err, result, fields) {
+                res.json(result);
+            })
         }
-    } else {
-        res.json({ error: 'Campo obrigatório não informado.' });
-    }
-})
 
-function aprovado (media){
+    })
+    .post((req, res) => {
+        const { matricula, nome, nota1, nota2 } = req.body;
+
+        if (matricula && nota1 && nota2 && nome) {
+            if (!isNaN(nota1) && !isNaN(nota2)) { // valida se as notas informadas são numéricas
+                const media = ((parseFloat(nota1) + parseFloat(nota2)) / 2);
+
+                pool.query(`INSERT INTO usuarios (Matricula, NomeCompleto, Nota1, Nota2, Media, Aprovado)
+                VALUES ('${matricula}','${nome}',${nota1},${nota2},${media},${aprovado()})`,
+                    function (err, result, fields) {
+                        if (err) { // se existir erro na query, retorna o erro para front com a mensagem do mysql
+                            res.status(500).json({ error: `${err.sqlMessage}` });
+                        } else { // se não tiver erro, retorna o status e a mensagem de sucesso.
+                            res.status(201).json({ message: `usuário criado!` });
+                        }
+                    })
+            } else {
+                res.status(400).json({ error: 'Nota inválida, deve ser numérica.' });
+            }
+        } else {
+            res.status(400).json({ error: 'Campo obrigatório não informado.' });
+        }
+    })
+    .put((req, res) => {
+        const { id, matricula, nome, nota1, nota2 } = req.body;
+        if (id && matricula && nome && nota1 && nota2) {
+            const media = calculaMedia(nota1, nota2);
+            if (media !== 0) {
+                pool.query(`UPDATE usuarios SET Matricula = '${matricula}', NomeCompleto = '${nome}', Nota1 = ${nota1}, Nota2 = ${nota2}, Media = ${media}, Aprovado = ${aprovado(media)} WHERE id = ${id}`,
+                    function (err, result, fields) {
+                        if (err) { // se existir erro na query, retorna o erro para front com a mensagem do mysql
+                            res.status(500).json({ error: `${err.sqlMessage}` });
+                        } else { // se não tiver erro, retorna o status e a mensagem de sucesso.
+                            res.status(201).json({ message: `usuário atualizado!` });
+                        }
+                    })
+            } else {
+                res.status(400).json({ error: 'Nota inválida, deve ser numérica.' });
+            }
+        } else {
+            res.status(400).json({ error: 'Campo obrigatório não informado.' });
+        }
+    })
+
+// função para validar se o usuário informado está aprovado
+function aprovado(media) {
     if (media >= 8) {
         return true;
     } else {
@@ -55,5 +81,13 @@ function aprovado (media){
     }
 }
 
+// função para calcular a média
+function calculaMedia(nota1, nota2, res) {
+    if (!isNaN(nota1) && !isNaN(nota2)) {
+        return ((parseFloat(nota1) + parseFloat(nota2)) / 2);
+    } else {
+        return 0;
+    }
+}
 
 module.exports = usuarios;
